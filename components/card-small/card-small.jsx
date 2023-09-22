0bf4aaac9c1c3e6419f4dacac785bacca2a88c5b
Kolpaxidis V <kolpaxidis86@gmail.com>
2023-09-22 11:52:57 +0300

    Add JSX compilation without framework or library. Add vite.config.js and dir "src" with pragma.ts. Pragrama.ts is preprocessinf function for JSX

---
+++ components/card-small/card-small.jsx
@@ -0,0 +1,15 @@
+import './card-small.scss';
+
+export const test = (
+	<div class="card-small">
+		<div class="slm-card-img">
+			<img src="/assets/images/chanise_cabbage.png" alt="chanise cabbage"></img>
+		</div>
+		<div class="slm-card-info">
+			<h3>Chanise Cabbage</h3>
+			<p>$14.99</p>
+			<div class="star-rating"></div>
+		</div>
+	</div>
+);
+
---
+++ main.js
@@ -0,0 +1,5 @@
+import { test } from "./components/card-small/card-small";
+
+const app = document.querySelector('#app');
+
+app.appendChild(test);
