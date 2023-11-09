0cfb81f1329f3f8ee9e289b16405c68a1b72a0c0
Kolpaxidis V <kolpaxidis86@gmail.com>
2023-11-09 12:51:44 +0200

    Change social component.

--- components/social/social.js
+++ components/social/social.js
@@ -1,30 +1,46 @@
-import socilaStyles from './social.module.scss';
+import socialStyles from './social.module.scss';
 
-export const showSocial = (
-	facebookTarget,
-	twitterTarget,
-	pinterestTarget,
-	instagramTarget,
-	facebookLink,
-	twitterLink,
-	pinterestLink,
-	instagramLink
-) => {
+
+const social_array = [
+	{
+		iconLink: './assets/icons/social/facebook.svg',
+		shareLink: 'https://www.facebook.com',
+		targetWindow: '_blank',
+		alt: 'facebook icon'
+	},
+	{
+		iconLink: './assets/icons/social/twitter.svg',
+		shareLink: 'https://twitter.com',
+		targetWindow: '_self',
+		alt: 'twitter icon'
+	},
+	{
+		iconLink: './assets/icons/social/pinterest.svg',
+		shareLink: 'https://www.pinterest.com',
+		targetWindow: '_blank',
+		alt: 'pinterest icon'
+	},
+	{
+		iconLink: './assets/icons/social/instagram.svg',
+		shareLink: 'https://www.instagram.com',
+		targetWindow: '_self',
+		alt: 'instagram icon'
+	}
+];
+
+export const drawSocial = () => {
+
+	const html = social_array.map((item) => {
+		return `
+		<a href="${item.shareLink}" target="${item.targetWindow}">
+			<img src="${item.iconLink}" alt="${item.alt}">
+		</a>`;
+	}).join('');
+
 
 	return `
-	<div class="${socilaStyles.socialContaner}">
-		<a href="${facebookLink}" target="${facebookTarget}">
-			<img src="./assets/icons/social/facebook.svg" alt="facebook icon">
-		</a>
-		<a href="${twitterLink}" target="${twitterTarget}">
-			<img src="./assets/icons/social/twitter.svg" alt="twitter icon">
-		</a>
-		<a href="${pinterestLink}" target="${pinterestTarget}">
-			<img src="./assets/icons/social/pinterest.svg" alt="pinterest icon">
-		</a>
-		<a href="${instagramLink}" target="${instagramTarget}">
-			<img src="./assets/icons/social/instagram.svg" alt="instagram icon">
-		</a>
-	</div>
-	`;
+    	<div class="${socialStyles.socialContaner}">
+			${html}
+    	</div>
+  `;
 };
