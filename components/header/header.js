264bc3732d4fda97ad0c4cd718bc0ac9d45e0107
Kolpaxidis V <kolpaxidis86@gmail.com>
2023-11-24 17:05:39 +0200

    selectInput component is done. Create new component header. Add new icons logo.svg and location.svg

---
+++ components/header/header.js
@@ -0,0 +1,11 @@
+import headerStyles from './header.module.scss';
+
+
+export const header = () => {
+
+	return `
+		<header>
+			<h1>Ecobazar</h1>
+		</header>
+	`;
+};
--- main.js
+++ main.js
@@ -15,7 +15,8 @@ const app = document.getElementById('app');
 // import { drawSocial } from './components/social/social';
 // import { textInput } from './components/inputs/text-input';
 // import { emailInput } from './components/inputs/email-input';
-import { selectInput } from './components/inputs/select-input';
+// import { selectInput } from './components/inputs/select-input';
+import { header } from './components/header/header';
 
 
 
@@ -37,12 +38,15 @@ import { selectInput } from './components/inputs/select-input';
 // app.appendChild(emailInput('id-223b', true, 'Enter your email'));
 // app.appendChild(selectInput('id-1_select', ['Tomato', 'Apples', 'Banana']));
 
-app.insertAdjacentHTML('beforeend', selectInput('id-1-select', ['Strawberry', 'Lemons', 'Grapes', 'Appples']));
-let selectRes;
-const selectElement = document.getElementById('id-1-select');
-const handlerSelectChange = (e) => {
-	const selectValue = e.target.value;
-	selectRes = selectValue;
-	console.log(selectRes);
-};
-selectElement.addEventListener('change', handlerSelectChange);
+// app.insertAdjacentHTML('beforeend', selectInput('id-1-select', ['Strawberry', 'Lemons', 'Grapes', 'Appples']));
+// let selectRes = 'Strawberry'; // set default value (must be a first value in array that u pass when call func)
+// const selectElement = document.getElementById('id-1-select');
+// const handlerSelectChange = (e) => {
+// 	const selectValue = e.target.value;
+// 	selectRes = selectValue;
+// 	console.log(selectRes);
+// };
+// console.log(selectRes);
+// selectElement.addEventListener('change', handlerSelectChange);
+
+app.insertAdjacentHTML('beforeend', header());
